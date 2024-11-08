2ff657e3acd9e64efbe5407856cef2c893f8e685
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-08 23:05:02 +0530

    created the movie_list api

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Movie
+from django.http import JsonResponse
 
-# Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = {'movies': list(movies.values())}
+    return JsonResponse(data)
