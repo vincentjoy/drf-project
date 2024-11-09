f52836ff83ae4ac2b31a18dbb75517777d8ca062
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-09 23:07:14 +0530

    added a model serializer

--- watchmate/watchlist_app/serializers.py
+++ watchmate/watchlist_app/serializers.py
@@ -1,6 +1,22 @@
 from rest_framework import serializers
 from .models import Movie
 
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and description cannot be the same.")
+        return data
+
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Name should be at least 3 characters long.")
+        return value
+
 # def description_length(value):
 #     if len(value) > 200:
 #         raise serializers.ValidationError("Description should be less than or equal to 200 characters.")
