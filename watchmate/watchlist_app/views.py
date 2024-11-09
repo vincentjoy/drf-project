2b1166261b09a0806f0c47444f7c68b482a6f896
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-09 09:40:44 +0530

    added a serializer and implemented the Get method in view

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render
 from .models import Movie
 from django.http import JsonResponse
+from .models import Movie
+from .serializers import MovieSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     data = {
+#         'movies': list(movies.values())
+#     }
+#     return JsonResponse(data)
+
+# def movie_details(request, pk):
+#     movie = Movie.objects.get(pk=pk)
+#     data = {
+#         'name': movie.name,
+#         'description': movie.description,
+#         'active': movie.active
+#     }
+#     return JsonResponse(data)
+
+
+@api_view()
 def movie_list(request):
     movies = Movie.objects.all()
-    data = {
-        'movies': list(movies.values())
-    }
-    return JsonResponse(data)
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
 
+
+@api_view()
 def movie_details(request, pk):
     movie = Movie.objects.get(pk=pk)
-    data = {
-        'name': movie.name,
-        'description': movie.description,
-        'active': movie.active
-    }
-    return JsonResponse(data)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
