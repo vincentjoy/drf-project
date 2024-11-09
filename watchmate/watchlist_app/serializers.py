cb13656c55562fbe92a6a3e7ddb8b58733c24398
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-09 22:35:03 +0530

    added field level, obect level and validator type validations

--- watchmate/watchlist_app/serializers.py
+++ watchmate/watchlist_app/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
 from .models import Movie
 
+def description_length(value):
+    if len(value) > 200:
+        raise serializers.ValidationError("Description should be less than or equal to 200 characters.")
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
-    description = serializers.CharField()
+    description = serializers.CharField(validators=[description_length])
     active = serializers.BooleanField()
 
     def create(self, validated_data):
@@ -16,3 +20,13 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and description cannot be the same.")
+        return data
+
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Name should be at least 3 characters long.")
+        return value
