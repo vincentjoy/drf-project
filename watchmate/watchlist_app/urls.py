552b3d38d89dda17a876df139d5b150fb7469e6a
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-10 13:01:08 +0530

    StreamingPlatformDetail view added

--- watchmate/watchlist_app/urls.py
+++ watchmate/watchlist_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import StreamingPlatformAV, WatchListAV, WatchDetailAV
+from .views import WatchListAV, WatchDetailAV, StreamingPlatformAV, StreamingPlatformDetailAV
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='watch_list'),
     path('<int:pk>/', WatchDetailAV.as_view(), name='details'),
     path('streamer', StreamingPlatformAV.as_view(), name='streaming_platform_list'),
+    path('streamer/<int:pk>', StreamingPlatformDetailAV.as_view(), name='streaming_platform_details'),
 ]
--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -20,6 +20,30 @@ class StreamingPlatformAV(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class StreamingPlatformDetailAV(APIView):
+    def get(self, request, pk):
+        try:
+            platform = StreamingPlatform.objects.get(pk=pk)
+        except StreamingPlatform.DoesNotExist:
+            return Response({'Error': 'Platform not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = StreamingPlatformSerializer(platform)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        platform = StreamingPlatform.objects.get(pk=pk)
+        serializer = StreamingPlatformSerializer(platform, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        platform = StreamingPlatform.objects.get(pk=pk)
+        platform.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 class WatchListAV(APIView):
     def get(self, request):
         movies = WatchList.objects.all()
