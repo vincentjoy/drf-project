e09dcf54e4f382fe66f5715c08d57ee5ffb53050
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-12 21:27:01 +0530

    token authentication settings added

--- watchmate/watchmate/urls.py
+++ watchmate/watchmate/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('watch/', include('watchlist_app.urls')),
-    path('api-auth/', include('rest_framework.urls')),
 ]
