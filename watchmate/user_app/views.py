57321d06f5b0302f86eddea5dcdf414d84cdc563
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-12 23:16:31 +0530

    added the registration process. testing pending

--- watchmate/user_app/views.py
+++ watchmate/user_app/views.py
@@ -1,13 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializers
+from rest_framework.authtoken.models import Token
 
 @api_view(['POST'])
 def registration_view(request):
     if request.method == 'POST':
-        serializer = RegistrationSerializer(data=request.data)
+        serializer = RegistrationSerializers(data=request.data)
+
+        data = {}
+        status = 201
+
         if serializer.is_valid():
-            serializer.save()
-            return Response({'message': 'Registration successful'}, status=201)
+            account = serializer.save()
+            data['message'] = 'Registration successful'
+            token = Token.objects.create(user=account)
+            data['token'] = token.key
+        else:
+            data = serializer.errors
+            status = 400
+
+        return Response(data, status=status)
 
 
