681a28dc644ce04ff4a2d91211b216850b181d0c
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-12 22:51:51 +0530

    added the registration serializer, url and view. Sending token in the registration response is pending

--- watchmate/user_app/urls.py
+++ watchmate/user_app/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
-
+from .views import registration_view
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
+    path('register/', registration_view, name='register'),
 ]
--- watchmate/user_app/views.py
+++ watchmate/user_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import RegistrationSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def registration_view(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Registration successful'}, status=201)
+
+
