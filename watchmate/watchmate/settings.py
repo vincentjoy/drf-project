4056d40e73b667e6a91da7d50300a2d7854efbd0
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-13 21:32:48 +0530

    applied global throttle to anonymous and logged in users

--- watchmate/watchmate/settings.py
+++ watchmate/watchmate/settings.py
@@ -129,5 +129,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.TokenAuthentication'
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1/day',
+        'user': '3/day'
+    }
 }
