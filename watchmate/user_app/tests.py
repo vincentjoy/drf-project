21067283ef415e664cd34e296355de871dba5c26
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-14 11:25:24 +0530

    added test cases for login and logout features

--- watchmate/user_app/tests.py
+++ watchmate/user_app/tests.py
@@ -20,3 +20,26 @@ class RegisterTestCase(APITestCase):
 
         user = User.objects.get(username='testuser')
         self.assertTrue(Token.objects.filter(user=user).exists())
+
+
+class LoginLogoutTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser',
+                                             password='testpassword')
+
+    def test_login(self):
+        data = {'username': 'testuser', 'password': 'testpassword'}
+        response = self.client.post(reverse('login'), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout_Second_Attempt(self):
+        self.token = Token.objects.get(user__username="testuser")
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key) # logging in the user
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
