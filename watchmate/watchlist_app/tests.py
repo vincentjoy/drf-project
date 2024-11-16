e24e1bfab2e5ed62c7c82e0a525601c3ea2705cf
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-16 18:19:14 +0530

    wrote the test case to test delete review functionality

--- watchmate/watchlist_app/tests.py
+++ watchmate/watchlist_app/tests.py
@@ -144,3 +144,7 @@ class ReviewTestCase(APITestCase):
     def test_review_user(self):
         response = self.client.get('/watch/reviews/?username'+ self.user.username)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_review_ind_delete(self):
+        response = self.client.delete(reverse('review_detail', args=(self.review.id,)))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
